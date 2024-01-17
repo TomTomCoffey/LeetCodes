bf3681f65179e6d8db3a1b9b5963b33db9bc97f0
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-01-17 17:55:21 -0500

    Cleaner code for Container With Most Water

--- Problem11.java
+++ Problem11.java
@@ -4,31 +4,26 @@ public class Problem11 {
 
     public int maxArea(int[] height) {
 
-        int maxVal = 0;
-        int right = height.length - 1;
+        int max = 0;
         int left = 0;
+        int right = height.length - 1;
 
-        while (right >= 0) {
-            int width = right - left;
+        while (left < right) {
 
-            if (height[left] >= height[right]) {
-                if (maxVal < width * height[right]) {
-                    maxVal = width * height[right];
-                }
+            if ((right - left) * Math.min(height[left], height[right]) > max) {
+                max = (right - left) * Math.min(height[left], height[right]);
+            }
+
+            if (height[left] > height[right]) {
                 right--;
-            } else if (height[right] > height[left]) {
-                if (maxVal < width * height[left]) {
-                    maxVal = width * height[left];
-                }
-                left++;
-
             } else {
-                return maxVal;
+                left++;
             }
 
         }
 
-        return maxVal;
+        return max;
+
     }
 
 }
